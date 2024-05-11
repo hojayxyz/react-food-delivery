14953fd962cac25a4a59ea55ccaefdc5bb633c05
hojay <jeahonglee@gmail.com>
2024-05-11 17:32:23 +0900

    Cart Context and reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
 
 function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +16,7 @@ function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
 import { currencyFormatter } from '../util/formatting';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
 
 function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -13,7 +21,7 @@ function MealItem({ meal }) {
           </p>
           <p className="meal-item-description">{meal.description}</p>
           <p className="meal-item-actions">
-            <Button>Add to Cart</Button>
+            <Button onClick={handleAddMealToCart}>Add to Cart</Button>
           </p>
         </div>
       </article>
